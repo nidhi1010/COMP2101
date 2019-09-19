ff7a8d2df66f4602cf8d3fd79d130e32b467250d
nidhi1010 <nidhi.sainikkr10@gmail.com>
2019-09-19 12:06:04 +0530

    final chagnes

--- bash/Helloworld.sh
+++ bash/Helloworld.sh
@@ -1,9 +1,5 @@
-#!bin/bash
-#echo "hello World!"
-#exit
 
-echo "setuid of files"
-
-find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
+echo "Setuid Files"
+sudo find / -type f -executable -perm -4000 -ls | sort -k 3
 
 
